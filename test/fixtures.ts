0b3a7e796377b7a5c25af7a8d3e49c13e64b6f85
Jonathan Lipps <jlipps@gmail.com>
2023-08-03 14:50:51 -0700

    chore: fix build

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,10 +1,8 @@
 import {JSONSchemaType} from 'ajv'
 import {resolve} from 'node:path'
-import * as url from 'node:url'
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-const FIXT_PATH = resolve(__dirname, 'fixtures')
+// can't use __dirname across cjs+esm, so just using a relative path for now
+const FIXT_PATH = resolve('test', 'fixtures')
 
 export const F1_PATH = resolve(FIXT_PATH, '1')
 export const F2_PATH = resolve(FIXT_PATH, '2')
